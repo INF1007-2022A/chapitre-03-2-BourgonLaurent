94e1c935d3c2bfeea78a56ff345fab8b42458583
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-09-11 15:31:35 -0400

    implement format_base

--- exercice.py
+++ exercice.py
@@ -43,16 +43,50 @@ def bills(value):
     return (twenties, tens, fives, ones)
 
 
-def format_base(value, base, digit_letters):
-    # Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
-    # `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
+def format_base(value: int, base: int, digit_letters: str):
+    """
+    Formater un nombre dans une base donnée en utilisant les lettres fournies pour les chiffres <
+    `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
+    """
     result = ""
     abs_value = abs(value)
-    while abs_value != 0:
-        pass
+
+    # Calculer quelle est la première base
+    # pour laquelle value est plus petite
+    max_exponent = 0
+    while abs_value >= base**max_exponent:
+        max_exponent += 1
+
+    # value ne va jamais dépasser base ** max_exponent
+    # alors on commence à celle en-dessous
+    curr_exp = max_exponent - 1
+
+    # Nombre de fois qu'on pourra soustraire
+    # la base actuelle
+    coeff = 0
+
+    while True:
+        if abs_value >= base**curr_exp:
+            coeff += 1
+            abs_value -= base**curr_exp
+        else:
+            # Ajouter le coefficient à la prochaine position
+            result += digit_letters[coeff]
+
+            # Puisque abs_value est toujours positif,
+            # une valeur nulle signifie que la
+            # valeur est totalement convertie
+            if abs_value == 0 and curr_exp == 0:
+                break
+
+            # Réinitialiser le compteur de base
+            coeff = 0
+            # Passer à la base inférieure
+            curr_exp -= 1
+
+    # Ajouter '-' devant pour les nombres négatifs.
     if value < 0:
-        # TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-        pass
+        result = "-" + result
     return result
 
 
